c4cf2e4afceb3ae1c919c9b004416716f96ad99d
arekbal <arek_bal@wp.pl>
2018-04-27 01:17:45 +0200

    build script

---
+++ build.ps1
@@ -0,0 +1,17 @@
+﻿
+$PROJECT_DIR = $env:PROJECT_DIR
+$BUILD_LOGGER = $env:BUILD_LOGGER
+
+if (-not $PROJECT_DIR) 
+{ 
+  $PROJECT_DIR = $PSScriptRoot 
+}
+
+$build = 'msbuild "$PROJECT_DIR\src\GherkinSpec.sln" /verbosity:minimal'
+
+if ($BUILD_LOGGER) 
+{ 
+  $build = $build + ' /logger:' + '"$BUILD_LOGGER"'
+} 
+
+iex $build
