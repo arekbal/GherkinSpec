b78e1b0327357647264333613934aafa1e6c95a6
arekbal <arek_bal@wp.pl>
2018-04-28 20:09:44 +0200

    gitversion basic config

---
+++ src/GherkinSpec.Core/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-unstable.41+Branch.dev.Sha.b7a7caee146baf4112c25ca927278afccad32688")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
